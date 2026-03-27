d280f2a395a9eb3fd98697412f6f986cfbb71ad0
Ernest Guevarra <ernest@guevarra.io>
2026-03-27 21:49:02 +0000

    update packages and R version 4.5.3

--- _targets.R
+++ _targets.R
@@ -1,50 +1,46 @@
-################################################################################
-#
-# General Targets Workflow
-#
-################################################################################
+# General Targets Workflow Template --------------------------------------------
 
-## Load libraries and custom functions -----------------------------------------
+
+## Load libraries and custom functions ----
 suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
-## Create targets and list targets objects -------------------------------------
 
-### Data targets
+## Data targets ----
 data_targets <- tar_plan(
   
 )
 
 
-### Processing targets
+## Processing targets ----
 processing_targets <- tar_plan(
   
 )
 
 
-### Analysis targets
+## Analysis targets ----
 analysis_targets <- tar_plan(
   
 )
 
 
-### Output targets
+## Output targets ----
 output_targets <- tar_plan(
   
 )
 
 
-### Reporting targets
+## Reporting targets ----
 report_targets <- tar_plan(
   
 )
 
 
-### Deploy targets
+## Deploy targets ----
 deploy_targets <- tar_plan(
   
 )
 
 
-## List targets
+## List targets ----
 all_targets()
